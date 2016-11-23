5e40c28904db8c0a48526d013c1eee56a108fcad
Steven Vandevelde <icid.asset@gmail.com>
2016-11-23 17:40:25 +0100

    Blocks

--- templates/Elements.hs
+++ templates/Elements.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RankNTypes #-}
 {-| Element functions you can use in the templates.
 
 -}
@@ -10,28 +11,30 @@ import Lucid.Base
 import Lucid.Html5
 import Types
 
-import qualified Data.Text as Text (replace)
+import qualified Data.Text as Text (empty, intercalate, pack, replace)
 
+
+block_ :: Element a
+block_ = make "div" "block"
+
+blockText_ :: Element a
+blockText_ = make "div" "block__text"
+
+blockTitle_ :: Element a
+blockTitle_ = make "h2" "block__title"
 
 blocks_ :: Element a
-blocks_ attributes =
-  with
-    (makeElement "div")
-    ([ class_ "blocks" ] ++ attributes)
-
+blocks_ = make "div" "blocks"
 
 blocksRow_ :: Element a
-blocksRow_ attributes =
-  with
-    (makeElement "div")
-    ([ class_ "blocks__row" ] ++ attributes)
-
+blocksRow_ = make "div" "blocks__row"
 
 container_ :: Element a
-container_ attributes =
-  with
-    (makeElement "div")
-    ([ class_ "container" ] ++ attributes)
+container_ = make "div" "container"
+
+
+
+-- Custom
 
 
 markdown_ :: Text -> Html ()
@@ -49,3 +52,23 @@ markdownWithoutBlocks_ markdown =
 
 use_ :: [Attribute] -> Html ()
 use_ = with (makeElementNoEnd "use")
+
+
+
+-- Helpers
+
+
+make :: String -> String -> Element a
+make tagName className =
+  let
+    className_ = Text.intercalate
+      " "
+      [ Text.empty
+      , Text.pack className
+      , Text.empty
+      ]
+  in
+    \attributes ->
+      with
+        (makeElement $ Text.pack tagName)
+        ([ class_ $ className_ ] ++ attributes)
