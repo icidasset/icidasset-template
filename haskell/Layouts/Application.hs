f0dcd08c0c8a0b24debe448e4e9760c4646fa33f
Steven Vandevelde <icid.asset@gmail.com>
2016-12-17 11:45:46 +0100

    Trying out a new style

--- haskell/Layouts/Application.hs
+++ haskell/Layouts/Application.hs
@@ -45,7 +45,7 @@ template obj children =
                 [ rel_ "stylesheet"
                 , href_ ( Text.append
                             "https://fonts.googleapis.com/css?family="
-                            "Playfair+Display|Montserrat:400,700|Lora:400,700,400italic,700italic"
+                            "Playfair+Display:400,700|Nunito+Sans:400,700,400italic,700italic"
                         )
                 ]
 
